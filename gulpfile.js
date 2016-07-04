2de478ad5702a38e5e18480dcfed7c718b1a21c5
Henrik Tenhovirta <henrik.tenhovirta@gofore.com>
2016-07-04 07:34:39 +0300

    add browsersync to dev pkg for liveupdates in the browser

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var atImport = require('postcss-import');
 var rename = require('gulp-rename');
 var cssnano = require('gulp-cssnano');
 var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
 
 gulp.task('css', function() {
   var processors = [atImport, cssnext({browsers: ['last 1 version']})];
@@ -14,5 +15,21 @@ gulp.task('css', function() {
     .pipe(gulp.dest('./dist/'))
     .pipe(cssnano())
     .pipe(rename('hui.min.css'))
-    .pipe(gulp.dest('./dist/'));
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browserSync.stream());
   });
+
+// Static server
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+    port: 3002
+  });
+  gulp.watch(['**/*.css'], ['css']);
+});
+
+gulp.task('default', function() {
+  runSequence(['serve']);
+});
